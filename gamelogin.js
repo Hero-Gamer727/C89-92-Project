5439c04192aabc2af719417590bec827d8d44e76
Hero-Gamer727 <83517823+Hero-Gamer727@users.noreply.github.com>
2021-09-11 11:41:02 -0500

    Add files via upload

--- gamelogin.js
+++ gamelogin.js
@@ -3,5 +3,5 @@ player1_name = document.getElementById("player1_name_input").value
 player2_name = document.getElementById("player2_name_input").value
 localStorage.setItem("player1name",player1_name);
 localStorage.setItem("player2name",player2_name);
-window.location = "gamepage.html"
+window.location = "index1.html"
 }
